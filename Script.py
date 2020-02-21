da37983b259d8fe7a617e858c6ace462b0de5186
xiaocheng-gao <xiaocheng.gao@outlook.com>
2020-02-21 13:14:02 +0100

    commit Script.py

--- Script.py
+++ Script.py
@@ -1,7 +1,34 @@
-from sklearn import datasets
-import matplotlib.pyplot as plt
+# try/except
+# with
+# class inheritance
+# generator
+# __eg__
+# --hash__
+# __str__
+# __repr__
+# __iter__
+# __next__
 
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
+class Employee:
 
+    num_of_emps = 0
+    raise_amount = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+        Employee.num_of_emps += 1
+
+    def fullname(self):
+        return "{} {}".format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Alan', 'Smith', 60000)
+
+print(Employee.num_of_emps)
